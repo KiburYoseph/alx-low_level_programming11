0ab50a8a7d1dd2c4f8954c3e3d979b104a0ee917
Kibur <kiburstgr@gemail>
2022-03-10 12:52:31 -0800

    dasd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	float f;
+	char c;
+	
+	printf("Size of a char: %lu.\n", sizeof(c));
+	printf("Size of an int: %lu.\n", sizeof(i));
+	printf("Size of a long int: %lu.\n", (long)sizeof(i));
+	printf("Size of a long long int: %lu.\n", (long long)sizeof(i));
+	printf("Size of a float: %lu.\n", sizeof(f));
+	return (0);
+}
