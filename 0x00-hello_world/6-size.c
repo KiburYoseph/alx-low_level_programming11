f35ca47136c438cd3c36617d4913164269cf2881
Kibur <kiburstgr@gemail>
2022-03-10 13:07:03 -0800

    asafas

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,11 @@ int main(void)
 	int i;
 	float f;
 	char c;
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long i));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long i));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+	printf("Size of a char: %2d byte(s)\n", sizeof(c));
+	printf("Size of an int: %2d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long i));
+	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long i));
+	printf("Size of a float: %2d byte(s)\n", sizeof(f));
 	return (0);
 }
