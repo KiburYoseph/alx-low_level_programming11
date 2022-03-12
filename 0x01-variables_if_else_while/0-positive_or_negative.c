60123cb7b627cd437e91d2036878908ded9288c8
Kibur <kiburstgr@gemail>
2022-03-11 17:05:19 -0800

    sdad

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * Main - Entry pont
+ * main - Entry pont
  * Return: 0 
  */
 int main(void)
@@ -17,7 +17,7 @@ int main(void)
 	else if(n < 0){
 	printf("%d is negative\n" , n);
 	}
-	else if((n = 0)){
+	else if(n == 0){
 	printf("%d is zero\n" , n);
 	}
 	return (0);
