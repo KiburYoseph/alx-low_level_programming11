bd852a51c58bcd14a025caa492c51080285bcc1b
Kibur <kiburstgr@gemail>
2022-03-15 17:56:30 -0700

    Sdad

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: 0
+ */
+void print_alphabet(void);
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
