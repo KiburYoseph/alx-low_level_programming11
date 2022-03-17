92b0ad929f1c601caa1e088130bb455524d82373
Kibur <kiburstgr@gemail>
2022-03-17 01:06:06 -0700

    sda

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -5,7 +5,22 @@
  * main -prints the largest of 3 integers
  * Return: 0
  */
+int largest_number(int a, int b, int c)
+{
+	int largest;
 
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 int main(void)
 {
 	int a, b, c;
