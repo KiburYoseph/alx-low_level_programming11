4d9c90e47f049e6b87d84cf9e220256398597e9b
Kibur <kiburstgr@gemail>
2022-03-22 06:37:39 -0700

    Asdad

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * swap_int- Swapping
+ * @a: variable
+ * @b: variable
  * Return: 0
  */
 
-int swap_int(int *a,int *b)
+int swap_int(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
