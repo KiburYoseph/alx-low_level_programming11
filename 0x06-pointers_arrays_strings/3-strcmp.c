f47e603007de25396ad412376d7742b14e1a5854
Kibur Yoseph <95487814+Kiburstgr@users.noreply.github.com>
2022-03-25 03:10:01 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,19 @@
  * _strcmp - compares two strings
  * @s1: string 1
  * @s2: string 2
- * Return: 1 if true, 0 if false
+ * Return: 0 if matching, and ns1 - ns2 if not matching
 */
 
 int _strcmp(char *s1, char *s2)
 {
-int equal = 0;
-while (*s1)
-{
-if (*s1 != *s2)
-{
-equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-break;
-}
-s1++;
-s2++;
-}
-return (equal);
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
