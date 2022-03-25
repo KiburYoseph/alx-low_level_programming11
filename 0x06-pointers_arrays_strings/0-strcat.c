3dd48db750ab35d4c8d8b95eddeb23157736cd87
Kibur Yoseph <95487814+Kiburstgr@users.noreply.github.com>
2022-03-25 03:14:12 +0300

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcat - concatinate two strings.
+* @dest: is destination.
+* @src: is source.
+*
+* Return: char dest.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0';)
+		++i;
+	for (j = 0; src[j] != '\0'; ++j, ++i)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
