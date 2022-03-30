22cd786c9f1a3a96c699850affa683e2fb9f9b92
Kiburstgr <kiburstgr@gmail.com>
2022-03-30 21:10:51 +0300

    sdad

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+/**
+* _print_rev_recursion- Reverses string
+**/
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		printf("%c", *s);
 	}
 }
