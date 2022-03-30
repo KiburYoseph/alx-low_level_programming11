da4c1f37b4a4a2be0258b3fa84aace907a04974a
Kibur Yoseph <95487814+Kiburstgr@users.noreply.github.com>
2022-03-30 21:47:22 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
-* power- raises
+* _pow_recursion- raises
 * @x: value
 * @y: value
 * Return: value
 **/
-int power(int x, int y) {
+int _pow_recursion(int x, int y) {
 if (y < 0)
 {
 return (-1);
 }
 else if (y != 0)
 {
-return (x * power(x, y - 1));
+return (x * _pow_recursion(x, y - 1));
 }
 else
 {
